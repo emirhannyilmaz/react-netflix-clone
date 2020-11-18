fe7ad97de53356fa6edc616b64159f6be4c82fca
Emirhan Yılmaz <emirhanra@gmail.com>
2020-11-18 18:05:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-netflix-clone
+
+### Demo:
+
+https://netflix-clone-dc8e1.web.app
+
+### Running Project:
+
+* Install dependencies: Run `npm install` in project's root directory
+* Get API key from TMDB and add your api key to src/requests.js
+* Run project: `npm start`
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow={false} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow={false} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow={false} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow={false} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow={false} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow={false} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} isLargeRow={false} />
+    </div>
+  );
+}
+
+export default App;
